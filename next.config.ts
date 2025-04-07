8e35f613f09c8c0a5d177a56c61d30998d5d9101
3Harang <jungjaeseung9475@gmail.com>
2025-04-07 15:30:35 +0900

    chore: tailwindcss v4->v3 다운그레이드 및 css variables 세팅

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'ext.same-assets.com',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
